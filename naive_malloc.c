41253b1c58e93519f437cb80df01e374ce2a10c7
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-01-20 22:59:53 -0800

    Naive malloc allocating by pages

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,11 +1,39 @@
 #include <error.h>
 #include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include "malloc.h"
 
 /**
- * naive_malloc - extend the program break to dynamically allocate memory
+ * _naive_malloc_sbrk - extend the program break to allocate memory
+ *
+ * @size: size to allocate
+ * @chunk: pointer to the start of the chunk
+ * @remaining: number of unused allocated bytes
+ *
+ * Return: If memory allocation fails, return NULL.
+ * Otherwise, return a pointer to the start of the allocated memory region.
+ */
+static void *_naive_malloc_sbrk(size_t size, void *chunk, size_t remaining)
+{
+	long page_size = sysconf(_SC_PAGESIZE);
+	size_t n_pages =
+		((size + 2 * sizeof(size_t) - remaining - 1) / page_size) + 1;
+
+	errno = 0;
+	if (sbrk(page_size * n_pages) == (void *) -1 && errno == ENOMEM)
+	{
+		error(0, ENOMEM, NULL);
+		return (NULL);
+	}
+	*((size_t *)(chunk)) = page_size * n_pages + remaining;
+	return (chunk);
+}
+
+
+/**
+ * naive_malloc - dynamically allocate memory
  *
  * @size: size to allocate
  *
@@ -17,31 +45,28 @@ void *naive_malloc(size_t size)
 	static void *start;
 	static size_t n_chunks;
 	unsigned char *chunk = NULL;
-	size_t unused_size = 0;
+	size_t remaining = 0;
 	size_t chunk_index = 0;
-	long page_size = 0;
 
 	if (n_chunks)
 	{
 		for (chunk = start; chunk_index < n_chunks; ++chunk_index)
-			chunk += *((size_t *)chunk);
-		unused_size = *((size_t *)chunk);
+			chunk += *((size_t *)(chunk));
+		remaining = *((size_t *)(chunk));
 	}
 	else
 	{
-		page_size = sysconf(_SC_PAGESIZE);
-		start = sbrk(0);
-		errno = 0;
-		if (sbrk(page_size) == (void *) -1 && errno == ENOMEM)
-		{
-			error(0, ENOMEM, NULL);
-			return (NULL);
-		}
-		chunk = start;
-		unused_size = page_size;
+		chunk = start = sbrk(0);
 	}
+	if (remaining < size + 2 * sizeof(size_t))
+		chunk = _naive_malloc_sbrk(size, chunk, remaining);
+	if (!chunk)
+		return (NULL);
 	n_chunks += 1;
-	*((size_t *)chunk) = size;
-	*((size_t *)chunk + size) = unused_size - size;
+	remaining = *((size_t *)(chunk));
+	*((size_t *)(chunk)) =
+		size + sizeof(size_t);
+	*((size_t *)(chunk + size + sizeof(size_t))) =
+		remaining - (size + sizeof(size_t));
 	return (chunk + sizeof(size_t));
 }
