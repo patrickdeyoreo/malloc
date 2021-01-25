83b482f4d0476ef6b8e67761bd929cc45e7f5e2e
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-01-24 20:51:58 -0800

    Added macros and fixed betty issue in malloc.h

--- malloc.h
+++ malloc.h
@@ -3,28 +3,43 @@
 
 #include <stddef.h>
 
-#define ALIGNMENT sizeof(void *)
-#define PADDING(n) (-(n) & (ALIGNMENT - 1))
-#define CHUNK_GET_SIZE(p) ((p)->size & ~1UL)
-#define CHUNK_GET_PREV(p) ((p)->prev)
-#define CHUNK_GET_USED(p) ((p)->size & 1)
-#define CHUNK_GET_FREE(p) ((p)->size & 1 ^ 1)
-#define CHUNK_SET_SIZE(p, n) ((p)->size = (n) | ((p)->size & 1))
-#define CHUNK_SET_PREV(p, n) ((p)->prev = (n))
-#define CHUNK_SET_USED(p) ((p)->size |= 1)
-#define CHUNK_SET_FREE(p) ((p)->size &= ~1UL)
+#define FOOT_SIZE (sizeof(size_t))
+#define HEAD_SIZE (sizeof(size_t))
+
+#define MIN_SIZE (FOOT_SIZE + HEAD_SIZE)
+#define TOP_SIZE MIN_SIZE
+
+#define ALIGNMENT (sizeof(void *))
+#define PADDING(n) ((ALIGNMENT - ((n) & (ALIGNMENT - 1))) & (ALIGNMENT - 1))
+#define ALIGNED(n) ((n) + PADDING(n))
+#define NEEDED(n) (MIN_SIZE + ALIGNED(n))
 
 /**
- * struct chunk_header_s - malloc chunk header
+ * struct chunk_s - malloc chunk header
  *
- * @prev_size: size of the previous chunk
- * @size: size of the chunk
+ * @prev_size: size of the previous chunk (only if not in-use)
+ * @size: size of the chunk and in-use bit of the previous chunk
+ * @next_free: pointer to the next free chunk in a circular doubly-linked list
+ * @prev_free: pointer to the prev free chunk in a circular doubly-linked list
  */
-typedef struct chunk_header_s
+typedef struct chunk_s
 {
-	size_t prev;
+	size_t prev_size;
 	size_t size;
-} chunk_header_t;
+	struct chunk_s *next_free;
+	struct chunk_s *prev_free;
+} chunk_t;
+
+#define CHUNK_PREV_USED_MASK ((size_t) 1)
+#define CHUNK_PREV_SIZE(p) ((p)->prev_size)
+#define CHUNK_SIZE(p) ((p)->size & ~CHUNK_PREV_USED_MASK)
+#define CHUNK_PREV_USED(p) ((p)->size & CHUNK_PREV_USED_MASK)
+#define CHUNK_NEXT(p) ((chunk_t *) ((char *) (p) + CHUNK_SIZE(p)))
+#define CHUNK_PREV(p) ((chunk_t *) ((char *) (p) - CHUNK_PREV_SIZE(p)))
+#define CHUNK_SET_PREV_SIZE(p, n) ((p)->prev_size = (n))
+#define CHUNK_SET_SIZE(p, n) ((p)->size = (n) | CHUNK_PREV_USED(p))
+#define CHUNK_SET_PREV_USED(p) ((p)->size |= CHUNK_PREV_USED_MASK)
+#define CHUNK_UNSET_PREV_USED(p) ((p)->size &= ~CHUNK_PREV_USED_MASK)
 
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
